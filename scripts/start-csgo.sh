6df653c60af58ef2ebb6b883040ddac423ca7918
mattieserver <mattijs-v@live.be>
2016-03-05 16:16:52 +0100

    Update start-csgo.sh

--- scripts/start-csgo.sh
+++ scripts/start-csgo.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-set -x
-ls $DATA_DIR
 if [ ! -f ${STEAMCMD_DIR}/steamcmd.sh ]; then
-    echo "File not found!"
+    echo "Steamcmd not found!"
+    cd $STEAMCMD_DIR
+    wget -q -O ${STEAMCMD_DIR}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
+    tar --directory ${STEAMCMD_DIR} -xvzf /serverdata/steamcmd/steamcmd_linux.tar.gz
+    rm ${STEAMCMD_DIR}/steamcmd_linux.tar.gz
+    chmod -R 774 ${STEAMCMD_DIR}/steamcmd.sh ${STEAMCMD_DIR}/linux32/steamcmd
 fi
 echo "---Update steamcmd---"
 ${STEAMCMD_DIR}/steamcmd.sh \

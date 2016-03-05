f6e1fe2b7d884a14941b942da81631b8bcef6412
mattieserver <mattijs-v@live.be>
2016-03-05 16:36:37 +0100

    added global game support

--- scripts/start-csgo.sh
+++ scripts/start-csgo.sh
@@ -7,21 +7,18 @@ if [ ! -f ${STEAMCMD_DIR}/steamcmd.sh ]; then
     rm ${STEAMCMD_DIR}/steamcmd_linux.tar.gz
     chmod -R 774 ${STEAMCMD_DIR}/steamcmd.sh ${STEAMCMD_DIR}/linux32/steamcmd
 fi
+
 echo "---Update steamcmd---"
 ${STEAMCMD_DIR}/steamcmd.sh \
     +login anonymous \
     +quit
+    
 echo "---Update server---"
 ${STEAMCMD_DIR}/steamcmd.sh \
     +login anonymous \
     +force_install_dir $SERVER_DIR \
-    +app_update 740 \
+    +app_update $GAME_ID \
     +quit
+    
 echo "---Start Server---"
-${SERVER_DIR}/srcds_run \
-    -game csgo -console -usercon \
-    +game_type $GAME_TYPE \
-    +game_mode $GAME_MODE \
-    +mapgroup $MAPGROUP \
-    +map $MAP  \
-    $OTHER
+${SERVER_DIR}/srcds_run  -game $GAME_NAME -console -usercon $GAME_PARAMS

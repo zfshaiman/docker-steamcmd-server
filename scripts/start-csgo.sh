c005115db33ebf69491e7f3300a6788aab494873
mattieserver <mattijs-v@live.be>
2016-03-05 14:46:54 +0100

    update script

--- scripts/start-csgo.sh
+++ scripts/start-csgo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-VOLUME=$DATA_DIR
 
-cd $STEAMCMD_DIR &&\
-    ./steamcmd.sh \
-        +login anonymous \
-        +force_install_dir $SERVER_DIR \
-        +app_update 740 \
-        +quit
+${STEAMCMD_DIR}/steamcmd.sh \
+    +login anonymous \
+    +force_install_dir $SERVER_DIR \
+    +app_update 740 \
+    +quit
 
-cd $SERVER_DIR &&\
-    ./srcds_run -game csgo -console -usercon +game_type $GAME_TYPE \
-                               +game_mode $GAME_MODE \
-                               +mapgroup $MAPGROUP \
-                               +map $MAP  $OTHER
+${SERVER_DIR}/srcds_run \
+    -game csgo -console -usercon \
+    +game_type $GAME_TYPE \
+    +game_mode $GAME_MODE \
+    +mapgroup $MAPGROUP \
+    +map $MAP  \
+    $OTHER

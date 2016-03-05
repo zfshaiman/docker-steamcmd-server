dbafddd69ce8174311c8eeb58f01acf8b81739d6
mattieserver <mattijs-v@live.be>
2016-03-05 15:52:47 +0100

    Update start-csgo.sh

--- scripts/start-csgo.sh
+++ scripts/start-csgo.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-
+volumes='ls $DATA_DIR'
+echo $volumes
+echo "---Update steamcmd---"
+${STEAMCMD_DIR}/steamcmd.sh \
+    +login anonymous \
+    +quit
+echo "---Update server---"
 ${STEAMCMD_DIR}/steamcmd.sh \
     +login anonymous \
     +force_install_dir $SERVER_DIR \
     +app_update 740 \
     +quit
-
+echo "---Start Server---"
 ${SERVER_DIR}/srcds_run \
     -game csgo -console -usercon \
     +game_type $GAME_TYPE \

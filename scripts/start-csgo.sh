8b26a5182fe57a565a1788ba13f5e861f060f1c0
mattieserver <mattijs-v@live.be>
2016-03-05 18:08:40 +0100

    Update start-csgo.sh

--- scripts/start-csgo.sh
+++ scripts/start-csgo.sh
@@ -21,4 +21,4 @@ ${STEAMCMD_DIR}/steamcmd.sh \
     +quit
     
 echo "---Start Server---"
-${SERVER_DIR}/srcds_run  -game $GAME_NAME -console -usercon $GAME_PARAMS
+${SERVER_DIR}/srcds_run -game $GAME_NAME -usercon $GAME_PARAMS

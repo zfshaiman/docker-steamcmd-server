f64ab2abd1c9f2d88836bca99caf6cd5e7407e77
ich777 <christophhummer@gmail.com>
2019-04-17 08:50:28 +0200

    Update start-server.sh
    fixed "steamclient.so" error

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -17,6 +17,10 @@ ${STEAMCMD_DIR}/steamcmd.sh \
     +force_install_dir $SERVER_DIR \
     +app_update $GAME_ID \
     +quit
-    
+
+echo "---Prepare Server---"
+mkdir ${DATA_DIR}/.steam/sdk32
+cp -R ${SERVER_DIR}/bin/* ${DATA_DIR}/.steam/sdk32/
+   
 echo "---Start Server---"
-${SERVER_DIR}/srcds_run -game $GAME_NAME -console $GAME_PARAMS +port $GAME_PORT
+${SERVER_DIR}/srcds_run -game $GAME_NAME $GAME_PARAMS -console +port $GAME_PORT

d4c4752c4cd5dd52e53d0ba3128a3b62b51bfaf2
ich777 <christophhummer@gmail.com>
2019-04-24 17:42:32 +0200

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -32,7 +32,6 @@ else
 fi
 
 echo "---Prepare Server---"
-mkdir -p ~/".local/share/Arma 3" && mkdir -p ~/".local/share/Arma 3 - Other Profiles"
 chmod -R 770 ${DATA_DIR}
 
 echo "---Start Server---"

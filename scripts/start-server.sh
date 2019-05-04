14754b8625a9f53cb007973c4d4b92f066787330
ich777 <christophhummer@gmail.com>
2019-05-04 15:56:49 +0200

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -49,6 +49,12 @@ else
 fi
 
 echo "---Prepare Server---"
+if [ ! -f ${SERVER_DIR}/left4dead/cfg/server.cfg ]; then
+    echo "---No server.cfg found, downloading...---"
+    wget -q -O $SERVER_DIR/left4dead/cfg/server.cfg https://raw.githubusercontent.com/ich777/docker-steamcmd-server/l4d/config/server.cfg
+else
+    echo "---server.cfg found..."
+fi
 chmod -R 770 ${DATA_DIR}
 echo "---Server ready---"
 

15e86a9a2b85dbf2d11f3fae0a0a0fe83834d28a
ich777 <christophhummer@gmail.com>
2019-05-07 19:34:11 +0200

    Update start-server.sh

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -51,10 +51,8 @@ fi
 echo "---Prepare Server---"
 chmod -R 770 ${DATA_DIR}
 
-sleep infinity
-
 echo "---Start Server---"
-${SERVER_DIR}/srcds_run -game ${GAME_NAME} ${GAME_PARAMS} +port ${GAME_PORT}
+${SERVER_DIR}/srcds_run -console ${GAME_PARAMS} -port ${GAME_PORT}
 
 
 

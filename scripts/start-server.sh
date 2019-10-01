728a3464ac05c3d990adf047ebd74bd054680924
ich777 <ich777@noreply.example.org>
2019-10-01 16:46:02 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -59,4 +59,4 @@ sleep infinity
 
 echo "---Start Server---"
 cd ${SERVER_DIR}
-${SERVER_DIR}/srcds_run -game ${GAME_NAME} ${GAME_PARAMS} -console +port ${GAME_PORT}
+${SERVER_DIR}/Unturned.x86_64 -nographics ${GAME_PARAMS} -sv

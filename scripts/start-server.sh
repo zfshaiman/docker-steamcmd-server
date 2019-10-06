b60393797888a8badc42ee8a350ae35d040876ae
ich777 <ich777@noreply.example.org>
2019-10-06 13:59:59 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+DL_URL_MULTIADMIN="$(curl -s https://api.github.com/repos/Grover-c13/MultiAdmin/releases/latest | grep browser_download_url | cut -d ":" -f 2,3 | cut -d '"' -f2)"
+DL_URL_SERVERMOD="$(curl -s https://api.github.com/repos/Grover-c13/Smod2/releases/latest | grep browser_download_url | cut -d ":" -f 2,3 | cut -d '"' -f2)"
+
 if [ ! -f ${STEAMCMD_DIR}/steamcmd.sh ]; then
     echo "SteamCMD not found!"
     wget -q -O ${STEAMCMD_DIR}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
@@ -22,14 +25,12 @@ if [ "${USERNAME}" == "" ]; then
     if [ "${VALIDATE}" == "true" ]; then
     	echo "---Validating installation---"
         ${STEAMCMD_DIR}/steamcmd.sh \
-        +@sSteamCmdForcePlatformType windows \
         +login anonymous \
         +force_install_dir ${SERVER_DIR} \
         +app_update ${GAME_ID} validate \
         +quit
     else
         ${STEAMCMD_DIR}/steamcmd.sh \
-        +@sSteamCmdForcePlatformType windows \
         +login anonymous \
         +force_install_dir ${SERVER_DIR} \
         +app_update ${GAME_ID} \
@@ -39,14 +40,12 @@ else
     if [ "${VALIDATE}" == "true" ]; then
     	echo "---Validating installation---"
         ${STEAMCMD_DIR}/steamcmd.sh \
-        +@sSteamCmdForcePlatformType windows \
         +login ${USERNAME} ${PASSWRD} \
         +force_install_dir ${SERVER_DIR} \
         +app_update ${GAME_ID} validate \
         +quit
     else
         ${STEAMCMD_DIR}/steamcmd.sh \
-        +@sSteamCmdForcePlatformType windows \
         +login ${USERNAME} ${PASSWRD} \
         +force_install_dir ${SERVER_DIR} \
         +app_update ${GAME_ID} \
@@ -54,13 +53,84 @@ else
     fi
 fi
 
+echo "---Installing MultiAdmin---"
+if [ "${VALIDATE}" == "true" ]; then
+	if [ -f ${SERVER_DIR}/MultiAdmin.exe ]; then
+    	rm  ${SERVER_DIR}/MultiAdmin.exe
+    fi
+    cd ${SERVER_DIR}
+	if wget -q $DL_URL_MULTIADMIN ; then
+		echo "---MultiAdmin sucessfully verified---"
+	else
+		echo "---Couldn't download MultiAdmin, putting server into sleep mode---"
+		sleep infinity
+	fi
+else
+    if [ ! -f ${SERVER_DIR}/MultiAdmin.exe ]; then
+    	cd ${SERVER_DIR}
+        if wget -q $DL_URL_MULTIADMIN ; then
+            echo "---MultiAdmin sucessfully installed---"
+        else
+            echo "---Couldn't download MultiAdmin, putting server into sleep mode---"
+            sleep infinity
+        fi
+    else
+        echo "---MultiAdmin found---"
+	fi
+fi
+
+echo "---Installing ServerMod---"
+if [ "${VALIDATE}" == "true" ]; then
+	if [ -f ${SERVER_DIR}/Assembly-CSharp.dll ]; then
+    	rm  ${SERVER_DIR}/Assembly-CSharp.dll
+    fi
+    if [ -f ${SERVER_DIR}/Smod2.dll ]; then
+    	rm  ${SERVER_DIR}/Smod2.dll
+    fi
+    cd ${SERVER_DIR}
+	if wget -q $DL_URL_SERVERMOD ; then
+		echo "---ServerMod sucessfully verified---"
+	else
+		echo "---Couldn't download ServerMod, putting server into sleep mode---"
+		sleep infinity
+	fi
+else
+	DL_URL_SERVERMOD=(${DL_URL_SERVERMOD[@]})
+    if [ ! -f ${SERVER_DIR}/Assembly-CSharp.dll ]; then
+    	echo "---File from ServerMod missing, installing---"
+    	cd ${SERVER_DIR}
+        if wget -q ${DL_URL_SERVERMOD[0]} ; then
+            echo "---Assembly-Csharp.dll sucessfully installed---"
+        else
+            echo "---Couldn't download Assembly-Csharp.dll, putting server into sleep mode---"
+            sleep infinity
+        fi
+    fi
+    if [ ! -f ${SERVER_DIR}/Smod2.dll ]; then
+    	echo "---File from ServerMod missing, installing---"
+    	cd ${SERVER_DIR}
+        if wget -q ${DL_URL_SERVERMOD[1]} ; then
+            echo "---Smod2.dll sucessfully installed---"
+        else
+            echo "---Couldn't download Smod2.dll, putting server into sleep mode---"
+            sleep infinity
+        fi
+	fi
+    if [ -f ${SERVER_DIR}/Assembly-CSharp.dll ] && [ -f ${SERVER_DIR}/Smod2.dll ]; then
+    	echo "---ServerMod found---"
+	fi
+fi
+
+
 echo "---Prepare Server---"
 chmod -R 770 ${DATA_DIR}
+echo "---Checking for old logs---"
+find ${SERVER_DIR} -name "masterLog.*" -exec rm -f {} \;
 echo "---Server ready---"
 
-echo "---Sleep zZz...---"
-sleep infinity
 
 echo "---Start Server---"
 cd ${SERVER_DIR}
-${SERVER_DIR}/srcds_run -game ${GAME_NAME} ${GAME_PARAMS} -console +port ${GAME_PORT}
+screen -S SCP -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m mono MultiAdmin.exe ${GAME_PARAMS}
+sleep 2
+tail -f ${SERVER_DIR}/masterLog.0

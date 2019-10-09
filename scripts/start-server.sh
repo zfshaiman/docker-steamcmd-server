793b0700f54adf5a3aae5bffead653310d1cff10
ich777 <ich777@noreply.example.org>
2019-10-09 09:01:14 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -80,9 +80,6 @@ echo "---Prepare Server---"
 chmod -R 770 ${DATA_DIR}
 echo "---Server ready---"
 
-echo "---Sleep zZz...---"
-sleep infinity
-
 echo "---Start Server---"
 cd ${SERVER_DIR}
 ${SERVER_DIR}/srcds_run -game ${GAME_NAME} ${GAME_PARAMS} -console +port ${GAME_PORT}

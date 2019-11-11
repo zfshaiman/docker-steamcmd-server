dd73febfc713405e8334029f93efd8a51ede73f8
ich777 <ich777@noreply.example.org>
2019-11-11 10:07:08 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -52,27 +52,30 @@ else
     fi
 fi
 
-#echo "---Prepare Server---"
-#echo "---Checking for 'Server.cfg'---"
-#if [ ! -f ${SERVER_DIR}/Server.cfg ]; then
-#	echo "---'Server.cfg' not found, downloading...---"
-#	cd ${SERVER_DIR}
-#    wget -qi Server.cfg https://raw.githubusercontent.com/ich777/docker-steamcmd-server/squad/config/Server.cfg
-#	if [ ! -f ${SERVER_DIR}/Server.cfg ]; then
-#		echo "-----------------------------------------------------------------------------------------"
-#		echo "---Something went wrong can't download 'Server.cfg' Putting server in sleep mode!---"
-#		echo "-----------------------------------------------------------------------------------------"
-#		sleep infinity
-#	fi
-#else
-#	echo "---'Server.cfg' folder found---"
-#fi
+if grep -rq 'ServerName="Squad Dedicated Server"' ${SERVER_DIR}/SquadGame/ServerConfig/Server.cfg; then
+	echo "---Standard 'Server.cfg' found, downloading custom---"
+	cd ${SERVER_DIR}/SquadGame/ServerConfig
+    if [ -f ${SERVER_DIR}/SquadGame/ServerConfig/Server.cfg ]; then
+    	mv ${SERVER_DIR}/SquadGame/ServerConfig/Server.cfg ${SERVER_DIR}/SquadGame/ServerConfig/Servercfg.bak
+	fi
+    wget -qi Server.cfg https://raw.githubusercontent.com/ich777/docker-steamcmd-server/squad/config/Server.cfg
+	if [ ! -f ${SERVER_DIR}/SquadGame/ServerConfig/Server.cfg ]; then
+		echo "-----------------------------------------------------------------------------------------"
+		echo "----Something went wrong can't download 'Server.cfg' falling back to standard config!----"
+		echo "-----------------------------------------------------------------------------------------"
+        if [ -f ${SERVER_DIR}/SquadGame/ServerConfig/Servercfg.bak ]; then
+			mv ${SERVER_DIR}/SquadGame/ServerConfig/Servercfg.bak ${SERVER_DIR}/SquadGame/ServerConfig/Server.cfg
+		fi
+	else
+    	echo "---Download of custom 'Server.cfg' successfull---"
+        if [ -f ${SERVER_DIR}/SquadGame/ServerConfig/Servercfg.bak ]; then
+    		rm ${SERVER_DIR}/SquadGame/ServerConfig/Servercfg.bak
+		fi
+	fi
+fi
 
 chmod -R 777 ${DATA_DIR}
 echo "---Server ready---"
-
-echo "---Sleep zZz...---"
-sleep infinity
 
 echo "---Start Server---"
 cd ${SERVER_DIR}

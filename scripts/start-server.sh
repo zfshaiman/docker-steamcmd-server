d2669c99c1d266d178ac4e5d8f3d694622e361b8
ich777 <ich777@noreply.example.org>
2019-11-11 11:20:44 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/bashecho "---Setting umask to ${UMASK}---"
+echo "---Setting umask to ${UMASK}---"
+umask ${UMASK}
+
 if [ ! -f ${STEAMCMD_DIR}/steamcmd.sh ]; then
     echo "SteamCMD not found!"
     wget -q -O ${STEAMCMD_DIR}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
@@ -62,11 +65,8 @@ else
     echo "---server.cfg found..."
 fi
 echo "---Please wait---"
-chmod -R 770 ${DATA_DIR}
+chmod -R 777 ${DATA_DIR}
 echo "---Server ready---"
 
 echo "---Start Server---"
 ${SERVER_DIR}/srcds_run -game ${GAME_NAME} ${GAME_PARAMS} +port ${GAME_PORT}
-
-
-

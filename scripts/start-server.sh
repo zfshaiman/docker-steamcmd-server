ab0bfe9d1b51158cc5ffad231ad8b8124dc4fbdc
ich777 <ich777@noreply.example.org>
2019-11-11 11:25:52 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "---Setting umask to ${UMASK}---"
+umask ${UMASK}
+
 if [ ! -f ${STEAMCMD_DIR}/steamcmd.sh ]; then
     echo "SteamCMD not found!"
     wget -q -O ${STEAMCMD_DIR}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
@@ -65,7 +68,7 @@ if [ ! -f ${SERVER_DIR}/storage/starbound_server.config ]; then
 else
 	echo "---Starbound server configuration found!---"
 fi
-chmod -R 770 ${DATA_DIR}
+chmod -R 777 ${DATA_DIR}
 echo "---Server ready---"
 
 echo "---Start Server---"

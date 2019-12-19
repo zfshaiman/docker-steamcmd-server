4fa86060a373a6ab66c4eed2a5faa741b239d2e7
ich777 <ich777@noreply.example.org>
2019-12-19 15:11:42 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -58,13 +58,14 @@ else
 fi
 
 echo "---Prepare Server---"
+export WINEARCH=win64
+export WINEPREFIX=/serverdata/serverfiles/WINE64
 echo "---Checking for 'saves' directory---"
 if [ ! -d ${SERVER_DIR}/saves ]; then
 	echo "---'saves' not found creating---"
     mkdir ${SERVER_DIR}/saves
 fi
 echo "---Directory 'saves' found!---"
-
 echo "---Checking for 'config.cfg'---"
 if [ ! -f ${SERVER_DIR}/config/config.cfg ]; then
 	echo "---'config.cfg' not found downloading---"
@@ -80,9 +81,27 @@ if [ ! -f ${SERVER_DIR}/config/config.cfg ]; then
         sleep infinity
     fi
 fi
+echo "---Checking if WINE workdirectory is present---"
+if [ ! -d ${SERVER_DIR}/WINE64 ]; then
+	echo "---WINE workdirectory not found, creating please wait...---"
+    mkdir ${SERVER_DIR}/WINE64
+else
+	echo "---WINE workdirectory found---"
+fi
+echo "---Checking if WINE is properly installed---"
+if [ ! -d ${SERVER_DIR}/WINE64/drive_C/windows ]; then
+	echo "---Setting up WINE---"
+    cd ${SERVER_DIR}
+    winecfg > /dev/null 2>&1
+    sleep 15
+else
+	echo "---WINE properly set up---"
+fi
+echo "---Checking for old display lock files---"
+find /tmp -name ".X99*" -exec rm -f {} \; > /dev/null 2>&1
 chmod -R 777 ${DATA_DIR}
 echo "---Server ready---"
 
 echo "---Start Server---"
 cd ${SERVER_DIR}
-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' wine ${SERVER_DIR}/TheForestDedicatedServer.exe -batchmode -dedicated -savefolderpath "${SERVER_DIR}/saves/" -configfilepath "${SERVER_DIR}/config/config.cfg" ${GAME_PARAMS}
+xvfb-run --auto-servernum --server-args='-screen 0 640x480x24:32' wine64 ${SERVER_DIR}/TheForestDedicatedServer.exe -batchmode -dedicated -savefolderpath "${SERVER_DIR}/saves/" -configfilepath "${SERVER_DIR}/config/config.cfg" ${GAME_PARAMS}

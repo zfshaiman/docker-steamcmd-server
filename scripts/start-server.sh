d4fe2d60a5541d39e793b78e2232f984f4fd681d
ich777 <ich777@noreply.example.org>
2020-02-25 14:39:18 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-echo "---Setting umask to ${UMASK}---"
-umask ${UMASK}
-
 if [ ! -f ${STEAMCMD_DIR}/steamcmd.sh ]; then
     echo "SteamCMD not found!"
     wget -q -O ${STEAMCMD_DIR}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
@@ -76,7 +73,7 @@ if [ ! -f ${SERVER_DIR}/Pavlov/Saved/Config/LinuxServer/Game.ini ]; then
 else
 	echo "---'Game.ini' found---"
 fi
-chmod -R 777 ${DATA_DIR}
+chmod -R ${DATA_PERM} ${DATA_DIR}
 echo "---Server ready---"
 
 echo "---Start Server---"

76ee4f1fa8459a1c74afa29a509bd8eff3d09306
ich777 <ich777@noreply.example.org>
2020-03-22 09:15:59 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -52,10 +52,21 @@ fi
 
 echo "---Prepare Server---"
 export DISPLAY=:99
-
+echo "---Checking if everything is in place---"
+if [ ! -f ${SERVER_DIR}/Configs/Network.eco ]; then
+	echo "---'Network.eco' not found, downloading---"
+	cd ${SERVER_DIR}/Configs
+	if wget -q -nc --show-progress --progress=bar:force:noscroll -O Network.eco https://raw.githubusercontent.com/ich777/docker-steamcmd-server/eco/config/Network.eco ; then
+		echo "---Successfully downloaded 'Network.eco'!---"
+	else
+		echo "---Something went wrong, can't download 'Network.eco', continuing---"
+	fi
+else
+	echo "---Everything in place---"
+fi
 echo "---Checking for old logfiles---"
-find $DATA_DIR -name "XvfbLog.*" -exec rm -f {} \;
-find $DATA_DIR -name "x11vncLog.*" -exec rm -f {} \;
+find ${SERVER_DIR} -name "XvfbLog.*" -exec rm -f {} \;
+find ${SERVER_DIR} -name "x11vncLog.*" -exec rm -f {} \;
 echo "---Checking for old lock files---"
 find /tmp -name ".X99*" -exec rm -f {} \;
 
@@ -63,21 +74,18 @@ chmod -R ${DATA_PERM} ${DATA_DIR}
 echo "---Server ready---"
 
 echo "---Starting Xvfb server---"
-screen -S Xvfb -L -Logfile ${DATA_DIR}/XvfbLog.0 -d -m /opt/scripts/start-Xvfb.sh
+screen -S Xvfb -L -Logfile ${SERVER_DIR}/XvfbLog.0 -d -m /opt/scripts/start-Xvfb.sh
 sleep 5
 
 if [ "${START_SRV_MGMT}" == "true" ]; then
 	echo "---Starting x11vnc server---"
-	screen -S x11vnc -L -Logfile ${DATA_DIR}/x11vncLog.0 -d -m /opt/scripts/start-x11.sh
+	screen -S x11vnc -L -Logfile ${SERVER_DIR}/x11vncLog.0 -d -m /opt/scripts/start-x11.sh
 	sleep 5
 
 	echo "---Starting noVNC server---"
 	websockify -D --web=/usr/share/novnc/ --cert=/etc/ssl/novnc.pem 8080 localhost:5900
 	sleep 5
 fi
-
-echo "---Sleep zZz---"
-sleep infinity
 
 echo "---Start Server---"
 cd ${SERVER_DIR}

a45eb640ef0cc997bb4b3ee263bede2b636d3c2b
ich777 <ich777@noreply.example.org>
2020-08-18 11:38:45 +0200

    „scripts/start.sh“ ändern

--- scripts/start.sh
+++ scripts/start.sh
@@ -16,9 +16,24 @@ else
 fi
 
 echo "---Starting...---"
+mkdir -p $DATA_DIR/".local/share/Arma 3" && mkdir -p $DATA_DIR/".local/share/Arma 3 - Other Profiles"
 chown -R ${UID}:${GID} /opt/scripts
 chown -R ${UID}:${GID} ${DATA_DIR}
 chown -R ${UID}:${GID} $DATA_DIR/.local
-chmod -R 770 $DATA_DIR/".local/share/Arma 3"
-chmod -R 770 $DATA_DIR/".local/share/Arma 3 - Other Profiles"
-su ${USER} -c "/opt/scripts/start-server.sh"
+chmod -R 770 ${DATA_DIR}/".local/share/Arma 3"
+chmod -R 770 ${DATA_DIR}/".local/share/Arma 3 - Other Profiles"
+
+term_handler() {
+	kill -SIGTERM "$killpid"
+	wait "$killpid" -f 2>/dev/null
+	exit 143;
+}
+
+trap 'kill ${!}; term_handler' SIGTERM
+su ${USER} -c "/opt/scripts/start-server.sh" &
+killpid="$!"
+while true
+do
+	wait $killpid
+	exit 0;
+done

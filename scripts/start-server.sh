d490df51659c1a1549f95c2f8431b902527a3617
ich777 <ich777@noreply.example.org>
2021-01-28 19:30:14 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -54,9 +54,6 @@ echo "---Prepare Server---"
 chmod -R ${DATA_PERM} ${DATA_DIR}
 echo "---Server ready---"
 
-echo "---Sleep zZz...---"
-sleep infinity
-
 echo "---Start Server---"
 cd ${SERVER_DIR}
-${SERVER_DIR}/srcds_run -game ${GAME_NAME} ${GAME_PARAMS} -console +port ${GAME_PORT}
+${SERVER_DIR}/Binaries/Linux/AAPG -servername=\"${SRV_NAME}\" ${GAME_PARAMS}

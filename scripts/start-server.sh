21ca2b18f6d7f4f7c9abc6640871819c718001ea
ich777 <ich777@noreply.example.org>
2021-01-30 08:03:28 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -129,6 +129,6 @@ echo "---Server ready---"
 
 echo "---Start Server---"
 cd ${SERVER_DIR}
-screen -S SCP -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m mono MultiAdmin.exe -multiadmin_nolog ${GAME_PARAMS}
+screen -S SCP -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m mono MultiAdmin.exe ${GAME_PARAMS}
 sleep 2
 tail -f ${SERVER_DIR}/masterLog.0

9ab218d3b35963d3dc302fc8337da15a53cb96b4
ich777 <ich777@noreply.example.org>
2021-02-11 15:10:16 +0100

    „scripts/start.sh“ ändern

--- scripts/start.sh
+++ scripts/start.sh
@@ -21,9 +21,8 @@ chown -R ${UID}:${GID} ${DATA_DIR}
 
 term_handler() {
 	echo 1 > ${SERVER_DIR}/server_exit.drp
-	sleep 3
-	kill -SIGTERM "$killpid"
-	wait "$killpid" -f 2>/dev/null
+	kill -SIGINT valheim
+	wait "$(pidof valheim)" -f 2>/dev/null
 	exit 143;
 }
 

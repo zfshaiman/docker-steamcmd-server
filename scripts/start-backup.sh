fa69a1055666029573d1895495a84c58513d0245
ich777 <ich777@noreply.example.org>
2021-02-23 20:52:12 +0100

    „scripts/start-backup.sh“ ändern

--- scripts/start-backup.sh
+++ scripts/start-backup.sh
@@ -5,6 +5,6 @@ do
 	cd ${SERVER_DIR}/.config/unity3d/IronGate/Valheim
 	tar -czf ${SERVER_DIR}/Backups/$(date '+%Y-%m-%d_%H.%M.%S').tar.gz .
 	cd ${SERVER_DIR}/Backups
-	ls -1tr ${SERVER_DIR}/Backups | head -n -${BACKUP_TO_KEEP} | xargs -d '\n' rm -f --
+	ls -1tr ${SERVER_DIR}/Backups | sort | head -n -${BACKUP_TO_KEEP} | xargs -d '\n' rm -f --
 	chmod -R ${DATA_PERM} ${SERVER_DIR}/Backups
 done

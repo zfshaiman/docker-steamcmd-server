52e492a27ee956a89f7be8f4fa970ec687b6ce8c
ich777 <ich777@noreply.example.org>
2021-03-03 10:02:29 +0100

    „scripts/start-updatecheck.sh“ ändern

--- scripts/start-updatecheck.sh
+++ scripts/start-updatecheck.sh
@@ -6,18 +6,18 @@ if [ -z "${UPDATE_CUR_V}" ]; then
 fi
 while true
 do
-	sleep ${UPDATE_CHECK_INTERVAL}m 
-	UPDATE_LAT_V="$(wget -qO- https://api.steamcmd.net/v1/info/896660 | jq -r '.data."'"$GAME_ID"'".depots.branches.public.buildid')"
+    sleep ${UPDATE_CHECK_INTERVAL}m 
+    UPDATE_LAT_V="$(wget -qO- https://api.steamcmd.net/v1/info/896660 | jq -r '.data."'"$GAME_ID"'".depots.branches.public.buildid')"
     if [ -z "${UPDATE_LAT_V}" ]; then
         echo "UPDATE CHECK: ---Something went wrong, can't get latest version of Valheim, trying again in ${UPDATE_CHECK_INTERVAL} minutes!---"
     elif [ "${UPDATE_CUR_V}" != "${UPDATE_LAT_V}" ]; then
         echo "UPDATE CHECK: ---New version of Valheim found, restarting and updating server in 10 seconds---"
         sleep 10
         pkill -SIGINT valheim
-	    wait "$(pidof valheim_server.x86_64)" -f 2>/dev/null
-	    sleep 4
-	    echo 1 > ${SERVER_DIR}/server_exit.drp
-	    exit 143;
+        wait "$(pidof valheim_server.x86_64)" -f 2>/dev/null
+        sleep 4
+        echo 1 > ${SERVER_DIR}/server_exit.drp
+        exit 143;
     else
         echo "UPDATE CHECK: ---Nothing to do, Valheim up-to-date---"
     fi

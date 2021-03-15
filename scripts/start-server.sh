9d67ff371e7d32e177576e4f9c9e38913d334123
ich777 <ich777@noreply.example.org>
2021-03-15 09:25:52 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -59,6 +59,16 @@ fi
 chmod -R ${DATA_PERM} ${DATA_DIR}
 screen -wipe 2&>/dev/null
 
+# Check if both ValheimPlus and BepInEx are enabled and throw error
+if [ "${ENABLE_VALHEIMPLUS}" == "true" ] && [ "${ENABLE_BEPINEX}" == "true" ]; then
+    echo
+    echo "---ValheimPlus and BepInEx are both enabled, only one of the two can---"
+    echo "---be enabled, please disable one of them and restart the container!---"
+    echo
+    echo "------------------Putting Container into sleep mode!-------------------"
+    sleep infinity
+fi
+
 if [ "${ENABLE_VALHEIMPLUS}" == "true" ]; then
     echo "---ValheimPlus enabled!---"
     CUR_V="$(find ${SERVER_DIR} -maxdepth 1 -name "ValheimPlus-*" | cut -d '-' -f2)"
@@ -103,6 +113,60 @@ if [ "${ENABLE_VALHEIMPLUS}" == "true" ]; then
         echo "---ValheimPlus v$CUR_V up-to-date---"
     fi
 fi
+
+if [ "${ENABLE_BEPINEX}" == "true" ]; then
+    echo "---BepInEx for Valheim enabled!---"
+    CUR_V="$(find ${SERVER_DIR} -maxdepth 1 -name "BepInEx-*" | cut -d '-' -f2)"
+    LAT_V="$(wget -qO- https://valheim.thunderstore.io/package/denikson/BepInExPack_Valheim/ | grep -A1 "Changelog" | tail -1 | cut -d '>' -f2 | cut -d '<' -f1)"
+    if [ -z "${LAT_V}" ] && [ -z "${CUR_V}" ]; then
+        echo "---Can't get latest version of BepInEx for Valheim!---"
+        echo "---Please try to run the Container without BepInEx for Valheim, putting Container into sleep mode!---"
+        sleep infinity
+    fi
+
+    if [ -f ${SERVER_DIR}/BepInEx.zip ]; then
+	    rm -rf ${SERVER_DIR}/BepInEx.zip
+    fi
+
+    echo "---BepInEx for Valheim Version Check---"
+    echo
+    echo "---https://valheim.thunderstore.io/package/denikson/BepInExPack_Valheim/---"
+    echo
+    if [ -z "${CUR_V}" ]; then
+        echo "---BepInEx for Valheim not found, downloading and installing v$LAT_V...---"
+        cd ${SERVER_DIR}
+        if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${SERVER_DIR}/BepInEx.zip --user-agent=Mozilla --content-disposition -E -c "https://valheim.thunderstore.io/package/download/denikson/BepInExPack_Valheim/$LAT_V/" ; then
+            echo "---Successfully downloaded BepInEx for Valheim v$LAT_V---"
+        else
+            echo "---Something went wrong, can't download BepInEx for Valheim v$LAT_V, putting container into sleep mode!---"
+            sleep infinity
+        fi
+        mkdir -p /tmp/BepInEx
+        unzip -o ${SERVER_DIR}/BepInEx.zip -d /tmp/BepInEx
+        cp -rf /tmp/BepInEx/BepInEx*/* ${SERVER_DIR}/
+        cp /tmp/BepInEx/README* ${SERVER_DIR}/README_BepInEx_for_Valheim
+	    touch ${SERVER_DIR}/BepInEx-$LAT_V
+        rm -rf ${SERVER_DIR}/BepInEx.zip /tmp/BepInEx
+    elif [ "$CUR_V" != "$LAT_V" ]; then
+        echo "---Version missmatch, BepInEx v$CUR_V installed, downloading and installing v$LAT_V...---"
+        cd ${SERVER_DIR}
+    	rm -rf ${SERVER_DIR}/BepInEx-$CUR_V
+        if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${SERVER_DIR}/BepInEx.zip --user-agent=Mozilla --content-disposition -E -c "https://valheim.thunderstore.io/package/download/denikson/BepInExPack_Valheim/$LAT_V/" ; then
+            echo "---Successfully downloaded BepInEx for Valheim v$LAT_V---"
+        else
+            echo "---Something went wrong, can't download BepInEx for Valheim v$LAT_V, putting container into sleep mode!---"
+            sleep infinity
+        fi
+        unzip -o ${SERVER_DIR}/BepInEx.zip -d /tmp/BepInEx
+        cp -rf /tmp/BepInEx/BepInEx*/* ${SERVER_DIR}/
+        cp /tmp/BepInEx/README* ${SERVER_DIR}/README_BepInEx_for_Valheim
+	    touch ${SERVER_DIR}/BepInEx-$LAT_V
+        rm -rf ${SERVER_DIR}/BepInEx.zip /tmp/BepInEx
+    elif [ "${CUR_V}" == "${LAT_V}" ]; then
+        echo "---BepInEx v$CUR_V up-to-date---"
+    fi
+fi
+
 echo "---Server ready---"
 
 if [ "${BACKUP_FILES}" == "true" ]; then
@@ -120,6 +184,22 @@ fi
 echo "---Start Server---"
 cd ${SERVER_DIR}
 if [ "${ENABLE_VALHEIMPLUS}" == "true" ]; then
+    export DOORSTOP_ENABLE=TRUE
+    export DOORSTOP_INVOKE_DLL_PATH=${SERVER_DIR}/BepInEx/core/BepInEx.Preloader.dll
+    export DOORSTOP_CORLIB_OVERRIDE_PATH=${SERVER_DIR}/unstripped_corlib
+    export LD_LIBRARY_PATH="${SERVER_DIR}/doorstop_libs":$LD_LIBRARY_PATH
+    export LD_PRELOAD=libdoorstop_x64.so:$LD_PRELOAD
+    export DYLD_LIBRARY_PATH="${SERVER_DIR}/doorstop_libs"
+    export DYLD_INSERT_LIBRARIES="${SERVER_DIR}/libdoorstop_x64.so"
+    export templdpath="$LD_LIBRARY_PATH"
+    export LD_LIBRARY_PATH=${SERVER_DIR}/linux64:"$LD_LIBRARY_PATH"
+    export SteamAppId=892970
+    if [ "${DEBUG_OUTPUT}" == "true" ]; then
+        ${SERVER_DIR}/valheim_server.x86_64 -name "${SRV_NAME}" -port ${GAME_PORT} -world "${WORLD_NAME}" -password "${SRV_PWD}" -public ${PUBLIC} ${GAME_PARAMS}
+    else
+        ${SERVER_DIR}/valheim_server.x86_64 -name "${SRV_NAME}" -port ${GAME_PORT} -world "${WORLD_NAME}" -password "${SRV_PWD}" -public ${PUBLIC} ${GAME_PARAMS} > /dev/null
+    fi
+elif [ "${ENABLE_BEPINEX}" == "true" ]; then
     export DOORSTOP_ENABLE=TRUE
     export DOORSTOP_INVOKE_DLL_PATH=${SERVER_DIR}/BepInEx/core/BepInEx.Preloader.dll
     export DOORSTOP_CORLIB_OVERRIDE_PATH=${SERVER_DIR}/unstripped_corlib

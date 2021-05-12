42289cdf228542175272755b206690adf51a3cca
ich777 <ich777@noreply.example.org>
2021-05-12 09:05:45 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -50,6 +50,52 @@ else
     fi
 fi
 
+if [ "${OXIDE_MOD}" == "true" ]; then
+  echo "---Oxide Mod enabled!---"
+  CUR_V="$(find ${SERVER_DIR} -maxdepth 1 -name rustinstalledv* | cut -d 'v' -f4-)"
+  LAT_V="$(wget -qO- https://api.github.com/repos/OxideMod/Oxide.Rust/releases/latest | grep tag_name | cut -d '"' -f4)"
+
+  if [ -z ${LAT_V} ]; then
+    if [ -z ${CUR_V} ]; then
+      echo "---Can't get latest Oxide Mod version and found no installed version, putting server into sleep mode!---"
+      sleep infinity
+    else
+      echo "---Can_t get latest Oxide Mod version, falling back to installed v${CUR_V}!---"
+      LAT_V="${CUR_V}"
+    fi
+  fi
+
+  if [ -z "$CUR_V" ]; then
+    echo "---Oxide Mod not found, downloading!---"
+    cd ${SERVER_DIR}
+    if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${SERVER_DIR}/OxideMod.zip "https://github.com/OxideMod/Oxide.Rust/releases/download/${LAT_V}/Oxide.Rust-linux.zip" ; then
+        echo "---Successfully downloaded Oxide Mode v${LAT_V}!---"
+    else
+        echo "---Something went wrong, can't download Oxide Mod v${LAT_V}, putting server in sleep mode---"
+        sleep infinity
+    fi
+    unzip -o ${SERVER_DIR}/OxideMod.zip -d ${SERVER_DIR}
+    touch ${SERVER_DIR}/rustinstalledv${LAT_V}
+    rm -rf ${SERVER_DIR}/OxideMod.zip
+  elif [ "$LAT_V" != "$CUR_V" ]; then
+    cd ${SERVER_DIR}
+    rm -rf ${SERVER_DIR}/rustinstalledv*
+    echo "---Newer version of Oxide Mod v${LAT_V} found, currently installed: v${CUR_V}---"
+    rm ${SERVER_DIR}/${JAR_NAME}.jar
+    if wget -q -nc --show-progress --progress=bar:force:noscroll -O ${SERVER_DIR}/OxideMod.zip "https://github.com/OxideMod/Oxide.Rust/releases/download/${LAT_V}/Oxide.Rust-linux.zip" ; then
+        echo "---Successfully downloaded Oxide Mod v${LAT_V}!---"
+    else
+        echo "---Something went wrong, can't download Oxide Mod v${LAT_V}, putting server in sleep mode---"
+        sleep infinity
+    fi
+    unzip -o ${SERVER_DIR}/OxideMod.zip -d ${SERVER_DIR}
+    touch ${SERVER_DIR}/rustinstalledv${LAT_V}
+    rm -rf ${SERVER_DIR}/OxideMod.zip
+  elif [ "$LAT_V" == "$CUR_V" ]; then
+    echo "---Oxide Mod v${CUR_V} is Up-To-Date!---"
+  fi
+fi
+
 echo "---Prepare Server---"
 chmod -R ${DATA_PERM} ${DATA_DIR}
 echo "---Setting Library path---"

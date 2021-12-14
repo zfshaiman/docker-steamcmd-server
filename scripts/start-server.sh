83992eb6cbd712bd280d0930db5e35f9a33c943d
ich777 <christophhummer@gmail.com>
2021-12-14 12:59:09 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -63,6 +63,9 @@ fi
 chmod -R ${DATA_PERM} ${DATA_DIR}
 echo "---Server ready---"
 
+echo "---Sleep zZzZz---"
+sleep infinity
+
 echo "---Start Server---"
 cd ${SERVER_DIR}
 ${SERVER_DIR}/srcds_run -game ${GAME_NAME} ${GAME_PARAMS} -console +port ${GAME_PORT}

05f752c244496660615220f1db21bf9696b78458
ich777 <christophhummer@gmail.com>
2022-02-03 10:05:23 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,30 +1,53 @@
 #!/bin/bash
 if [ ! -f ${STEAMCMD_DIR}/steamcmd.sh ]; then
-    echo "Steamcmd not found!"
+    echo "SteamCMD not found!"
     wget -q -O ${STEAMCMD_DIR}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
     tar --directory ${STEAMCMD_DIR} -xvzf /serverdata/steamcmd/steamcmd_linux.tar.gz
     rm ${STEAMCMD_DIR}/steamcmd_linux.tar.gz
 fi
 
-echo "---Update steamcmd---"
-${STEAMCMD_DIR}/steamcmd.sh \
-    +login anonymous \
-    +quit
-    
-echo "---Update server---"
-if [ "${VALIDATE}" == "true" ]; then
-	echo "---Validating installation---"
+echo "---Update SteamCMD---"
+if [ "${USERNAME}" == "" ]; then
     ${STEAMCMD_DIR}/steamcmd.sh \
-    +force_install_dir ${SERVER_DIR} \
     +login anonymous \
-    +app_update ${GAME_ID} validate \
     +quit
 else
     ${STEAMCMD_DIR}/steamcmd.sh \
-    +force_install_dir ${SERVER_DIR} \
-    +login anonymous \
-    +app_update ${GAME_ID} \
+    +login ${USERNAME} ${PASSWRD} \
     +quit
+fi
+
+echo "---Update Server---"
+if [ "${USERNAME}" == "" ]; then
+    if [ "${VALIDATE}" == "true" ]; then
+    	echo "---Validating installation---"
+        ${STEAMCMD_DIR}/steamcmd.sh \
+        +force_install_dir ${SERVER_DIR} \
+        +login anonymous \
+        +app_update ${GAME_ID} validate \
+        +quit
+    else
+        ${STEAMCMD_DIR}/steamcmd.sh \
+        +force_install_dir ${SERVER_DIR} \
+        +login anonymous \
+        +app_update ${GAME_ID} \
+        +quit
+    fi
+else
+    if [ "${VALIDATE}" == "true" ]; then
+    	echo "---Validating installation---"
+        ${STEAMCMD_DIR}/steamcmd.sh \
+        +force_install_dir ${SERVER_DIR} \
+        +login ${USERNAME} ${PASSWRD} \
+        +app_update ${GAME_ID} validate \
+        +quit
+    else
+        ${STEAMCMD_DIR}/steamcmd.sh \
+        +force_install_dir ${SERVER_DIR} \
+        +login ${USERNAME} ${PASSWRD} \
+        +app_update ${GAME_ID} \
+        +quit
+    fi
 fi
 
 echo "---Prepare Server---"
